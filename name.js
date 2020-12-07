c2deeaa90978dcaeaade33e50eeed8a3ae2a8f2c
Camden Mecklem <c@camdenmecklem.com>
2020-12-07 18:30:18 -0500

    add content

---
+++ name.js
@@ -0,0 +1,17 @@
+// const nameDisplay = document.getElementById('name')
+
+// let tracker = 0
+
+// const nameTicker = setInterval(() => {
+//   nameDisplay.innerHTML = name[tracker]
+//   tracker += 1
+
+//   if (tracker >= name.length) {
+//     clearInterval(nameTicker)
+//   }
+// }, 500)
+
+// const name = 'Kauthar'
+
+// nameTicker(name)
+
